f6ee8430cfe740667ebf36593386c202ed2ff4b9
Jaren <jarenmchugh@gmail.com>
2025-12-13 15:23:41 -0600

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "AITX Hackathon 2025 - Next.js development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_20
+            nodePackages.npm
+            nodePackages.pnpm
+            nodePackages.yarn
+          ];
+
+          shellHook = ''
+            echo "Node.js version: $(node --version)"
+            echo "npm version: $(npm --version)"
+          '';
+        };
+      }
+    );
+}
